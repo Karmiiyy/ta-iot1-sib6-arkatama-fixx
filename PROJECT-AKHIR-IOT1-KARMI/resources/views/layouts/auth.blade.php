a7039817126d0a7d8e99a2b8544f506d9ea3d753
Karmi <karmiduddin@gmail.com>
2024-06-01 00:32:22 +0700

    COMMIT KE GITHUB

---
+++ PROJECT-AKHIR-IOT1-KARMI/resources/views/layouts/auth.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @include('layouts.dashboard._head')
+</head>
+
+<body>
+    @yield('content')
+    <!-- content-wrapper ends -->
+    @include('layouts.dashboard._foot')
+
+</body>
+
+</html>
