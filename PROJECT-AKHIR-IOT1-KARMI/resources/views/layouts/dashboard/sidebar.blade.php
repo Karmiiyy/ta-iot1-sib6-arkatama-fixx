f3aed356fcabf5c892b67b1f2e95e0e5241d8c7b
Karmi <karmiduddin@gmail.com>
2024-06-01 08:57:21 +0700

    COMMITTT

---
+++ PROJECT-AKHIR-IOT1-KARMI/resources/views/layouts/dashboard/sidebar.blade.php
@@ -0,0 +1,36 @@
+<div class="iq-sidebar">
+  <div class="iq-sidebar-logo d-flex justify-content-between">
+    <a href="index.html">
+      <img src="images/logo-1.png.png" class="img-fluid" alt="">
+      <span>JK</span>
+    </a>
+    <div class="iq-menu-bt align-self-center">
+      <div class="wrapper-menu">
+        <div class="line-menu half start"></div>
+        <div class="line-menu"></div>
+        <div class="line-menu half end"></div>
+      </div>
+    </div>
+  </div>
+  <div id="sidebar-scrollbar">
+    <nav class="iq-sidebar-menu">
+      <ul id="iq-sidebar-toggle" class="iq-menu">
+        <li class="iq-menu-title"><i class="ri-separator"></i><span>Navigasi</span></li>
+        <li>
+          <a href="dashboard.html" class="iq-waves-effect"><i class="ri-home-4-line"></i><span>Dashboard</span></a>
+        </li>
+        <li>
+          <a href="user.html" class="iq-waves-effect"><i class="ri-user-line"></i><span>User</span></a>
+        </li>
+        <li>
+          <a href="led-control.html" class="iq-waves-effect"><i class="ri-lightbulb-line"></i><span>LED
+              Control</span></a>
+        </li>
+        <li>
+          <a href="sensor.html" class="iq-waves-effect"><i class="ri-sensor-line"></i><span>Sensor</span></a>
+        </li>
+      </ul>
+    </nav>
+    <div class="p-3"></div>
+  </div>
+</div>
